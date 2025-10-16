c9a3d813499db959b5620bbfdc38f3dd62fdce1e
msang1411 <harley20005@gmail.com>
2025-10-16 13:21:37 +0700

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const characters = [
   { name: "Kei", role: "Boss", avatar: "👩‍💼" },
   { name: "Emma", role: "Developer", avatar: "👩‍💻" },
-  { name: "Mia", role: "Human Resource management", avatar: "👩‍🎨" },
+  { name: "Mia", role: "Human Resource Management", avatar: "👩‍🎨" },
   { name: "Jane", role: "Personal Assistant (PA)", avatar: "👩‍⚕️" },
   { name: "Annie", role: "Quality Assurance (QA)", avatar: "👩‍🚀" },
   { name: "Flora", role: "Quality Assurance (QA)", avatar: "👩‍🍳" },
